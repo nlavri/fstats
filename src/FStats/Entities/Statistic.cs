930c12cf43a24b11150aeff0b4352f0deff86ee5
nlavri <nlavri@gmail.com>
2017-06-12 13:42:28 +0300

    db

---
+++ src/FStats/Entities/Statistic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FStats.Entities
+{
+    public partial class Statistic
+    {
+        public string Div { get; set; }
+        public string Date { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public string Fthg { get; set; }
+        public string Ftag { get; set; }
+        public string Ftr { get; set; }
+        public string Hthg { get; set; }
+        public string Htag { get; set; }
+        public string Htr { get; set; }
+        public string Referee { get; set; }
+        public string Hs { get; set; }
+        public string As { get; set; }
+        public string Hst { get; set; }
+        public string Ast { get; set; }
+        public string Hf { get; set; }
+        public string Af { get; set; }
+        public string Hc { get; set; }
+        public string Ac { get; set; }
+        public string Hy { get; set; }
+        public string Ay { get; set; }
+        public string Hr { get; set; }
+        public string Ar { get; set; }
+        public string B365h { get; set; }
+        public string B365d { get; set; }
+        public string B365a { get; set; }
+        public string Bwh { get; set; }
+        public string Bwd { get; set; }
+        public string Bwa { get; set; }
+        public string Iwh { get; set; }
+        public string Iwd { get; set; }
+        public string Iwa { get; set; }
+        public string Lbh { get; set; }
+        public string Lbd { get; set; }
+        public string Lba { get; set; }
+        public string Psh { get; set; }
+        public string Psd { get; set; }
+        public string Psa { get; set; }
+        public string Whh { get; set; }
+        public string Whd { get; set; }
+        public string Wha { get; set; }
+        public string Vch { get; set; }
+        public string Vcd { get; set; }
+        public string Vca { get; set; }
+        public string Bb1X2 { get; set; }
+        public string BbMxH { get; set; }
+        public string BbAvH { get; set; }
+        public string BbMxD { get; set; }
+        public string BbAvD { get; set; }
+        public string BbMxA { get; set; }
+        public string BbAvA { get; set; }
+        public string BbOu { get; set; }
+        public string BbMx251 { get; set; }
+        public string BbAv251 { get; set; }
+        public string BbMx25 { get; set; }
+        public string BbAv25 { get; set; }
+        public string BbAh { get; set; }
+        public string BbAhh { get; set; }
+        public string BbMxAhh { get; set; }
+        public string BbAvAhh { get; set; }
+        public string BbMxAha { get; set; }
+        public string BbAvAha { get; set; }
+        public string Psch { get; set; }
+        public string Pscd { get; set; }
+        public string Psca { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ src/FStats/Entities/StatsDbContext.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FStats.Entities
+{
+    public partial class StatsDbContext : DbContext
+    {
+        public virtual DbSet<Statistic> Statistic { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=fstats;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Statistic>(entity =>
+            {
+                entity.Property(e => e.Ac)
+                    .HasColumnName("AC")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Af)
+                    .HasColumnName("AF")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Ar)
+                    .HasColumnName("AR")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.As)
+                    .HasColumnName("AS")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Ast)
+                    .HasColumnName("AST")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.AwayTeam).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Ay)
+                    .HasColumnName("AY")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.B365a)
+                    .HasColumnName("B365A")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.B365d)
+                    .HasColumnName("B365D")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.B365h)
+                    .HasColumnName("B365H")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Bb1X2).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAh)
+                    .HasColumnName("BbAH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAhh)
+                    .HasColumnName("BbAHh")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAv25)
+                    .HasColumnName("BbAv<2 5")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAv251)
+                    .HasColumnName("BbAv>2 5")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAvA).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAvAha)
+                    .HasColumnName("BbAvAHA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAvAhh)
+                    .HasColumnName("BbAvAHH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAvD).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbAvH).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbMx25)
+                    .HasColumnName("BbMx<2 5")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbMx251)
+                    .HasColumnName("BbMx>2 5")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbMxA).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbMxAha)
+                    .HasColumnName("BbMxAHA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbMxAhh)
+                    .HasColumnName("BbMxAHH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbMxD).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbMxH).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.BbOu)
+                    .HasColumnName("BbOU")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Bwa)
+                    .HasColumnName("BWA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Bwd)
+                    .HasColumnName("BWD")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Bwh)
+                    .HasColumnName("BWH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Date).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Div).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Ftag)
+                    .HasColumnName("FTAG")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Fthg)
+                    .HasColumnName("FTHG")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Ftr)
+                    .HasColumnName("FTR")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Hc)
+                    .HasColumnName("HC")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Hf)
+                    .HasColumnName("HF")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.HomeTeam).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Hr)
+                    .HasColumnName("HR")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Hs)
+                    .HasColumnName("HS")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Hst)
+                    .HasColumnName("HST")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Htag)
+                    .HasColumnName("HTAG")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Hthg)
+                    .HasColumnName("HTHG")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Htr)
+                    .HasColumnName("HTR")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Hy)
+                    .HasColumnName("HY")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Iwa)
+                    .HasColumnName("IWA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Iwd)
+                    .HasColumnName("IWD")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Iwh)
+                    .HasColumnName("IWH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Lba)
+                    .HasColumnName("LBA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Lbd)
+                    .HasColumnName("LBD")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Lbh)
+                    .HasColumnName("LBH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Psa)
+                    .HasColumnName("PSA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Psca)
+                    .HasColumnName("PSCA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Pscd)
+                    .HasColumnName("PSCD")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Psch)
+                    .HasColumnName("PSCH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Psd)
+                    .HasColumnName("PSD")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Psh)
+                    .HasColumnName("PSH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Referee).HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Vca)
+                    .HasColumnName("VCA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Vcd)
+                    .HasColumnName("VCD")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Vch)
+                    .HasColumnName("VCH")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Wha)
+                    .HasColumnName("WHA")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Whd)
+                    .HasColumnName("WHD")
+                    .HasColumnType("varchar(50)");
+
+                entity.Property(e => e.Whh)
+                    .HasColumnName("WHH")
+                    .HasColumnType("varchar(50)");
+            });
+        }
+    }
+}
